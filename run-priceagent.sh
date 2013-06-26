2fb4014f8d89aba3489bea3380a8613335f848bc
Norman Seidel <norman.seidel@googlemail.com>
2013-06-27 00:04:38 +0200

    shell script to execute priceagent

---
+++ run-priceagent.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#
+# Shell script to run price agent from a console
+#
+##################################################
+
+export PATH_TO_PRICEAGENT=
+
+php -f $PATH_TO_PRICEAGENT/priceagent.php
